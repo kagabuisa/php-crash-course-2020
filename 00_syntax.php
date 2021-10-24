8d5bfdb1e7aa185debac87a4b121522172aac73d
Isa Kagabu <kagabuisa@gmail.com>
2021-10-24 10:44:12 +0300

    first commit

---
+++ 00_syntax.php
@@ -0,0 +1,26 @@
+<!doctype html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport"
+        content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+  <meta http-equiv="X-UA-Compatible" content="ie=edge">
+  <title>Document</title>
+</head>
+<body>
+<?php
+echo "hello world Kagabu Isa";
+?>
+<?php
+echo "Kibuli SSssss";
+?>
+<?php
+$statt =  '<a>'.'<br>'."hello muganzi multiple string".'<br>';
+echo str_repeat($statt,3);
+echo $statt;
+?>
+<?php
+echo "hello Kampala";
+?>
+</body>
+</html>
